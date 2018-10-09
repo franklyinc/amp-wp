34c9d2626f432b445671195df2475c793a3cf196
Maksim Parfeniuk <mparfeniuk@gmail.com>
2018-10-09 15:23:44 +0300

    WEB-5235 - gmaps src attribute fix

--- frankly-amp.php
+++ frankly-amp.php
@@ -11,6 +11,12 @@ require_once 'includes/utils/frankly-amp-utils.php';
  */
 require_once 'amp.php';
 
+
+/**
+ * Remove core classes
+ */
+AMP_Autoloader::remove_autoload_class("AMP_Tag_And_Attribute_Sanitizer");
+
 /**
  * Register custom classes
  */
@@ -19,6 +25,8 @@ AMP_Autoloader::register_autoload_class( 'Frankly_AMP_Facebook_Embed_Handler', '
 AMP_Autoloader::register_autoload_class( 'Frankly_AMP_Instagram_Embed_Handler', 'includes/embeds/frankly-amp-embeds/class-frankly-amp-instagram-embed' );
 AMP_Autoloader::register_autoload_class( 'Frankly_AMP_Video_Embed_Handler', 'includes/embeds/frankly-amp-embeds/class-frankly-amp-video-embed' );
 AMP_Autoloader::register_autoload_class( 'Frankly_AMP_UB_Embed_Handler', 'includes/embeds/frankly-amp-embeds/class-frankly-amp-ub-embed' );
+AMP_Autoloader::register_autoload_class( 'Frankly_AMP_P_Tag_Sanitizer', 'includes/sanitizers/frankly-amp-sanitizers/class-frankly-amp-p-tag-sanitizer' );
+AMP_Autoloader::register_autoload_class( 'Frankly_AMP_Tag_And_Attribute_Sanitizer', 'includes/sanitizers/frankly-amp-sanitizers/class-frankly-amp-tag-and-attribute-sanitizer' );
 
 $html = file_get_contents( 'php://input' );
 
@@ -32,10 +40,14 @@ $embed_handlers['Frankly_AMP_Instagram_Embed_Handler'] = array();
 $embed_handlers['Frankly_AMP_Video_Embed_Handler'] = array();
 $embed_handlers['Frankly_AMP_UB_Embed_Handler'] = array();
 
+$sanitizer_handlers = amp_get_content_sanitizers( $html );
+$sanitizer_handlers["Frankly_AMP_P_Tag_Sanitizer"] = array();
+$sanitizer_handlers['Frankly_AMP_Tag_And_Attribute_Sanitizer'] = array();
+
 $amp_content = new AMP_Content(
 	$html,
 	$embed_handlers,
-	amp_get_content_sanitizers( $html ),
+	$sanitizer_handlers,
 	array(
 		'content_max_width' => 400,
 	)
